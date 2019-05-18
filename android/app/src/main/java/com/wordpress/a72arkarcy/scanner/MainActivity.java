075520543008962c7e3a33a7335429b55f5d7f5c
harsh <harshsuvagiya24@gmail.com>
2019-05-18 15:01:36 +0530

    Text-Recognizer and Label detector using flutter

---
+++ android/app/src/main/java/com/wordpress/a72arkarcy/scanner/MainActivity.java
@@ -0,0 +1,14 @@
+package com.wordpress.a72arkarcy.scanner;
+
+import android.os.Bundle;
+
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
